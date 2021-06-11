4f8b947fb3b2da0a199734b4684b2a0427ebd1d8
yudong <1016724958@qq.com>
2021-06-11 16:21:45 +0800

    添加项目文件。

---
+++ WPFCustomCursorDemo/CursorHelper.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media.Imaging;
+using Brush = System.Drawing.Brush;
+using Brushes = System.Drawing.Brushes;
+using Pen = System.Drawing.Pen;
+using Rectangle = System.Drawing.Rectangle;
+
+namespace WPFCustomCursorDemo
+{
+    public static class CursorHelper
+    {
+        public static Cursor CreateFillCursor(int size = 24, Brush fillBrush = null)
+        {
+            int unitSize = size / 4;
+            var bmp = new Bitmap(size, size);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clip = new Region(new Rectangle(0, 0, size, size));
+                g.SmoothingMode = SmoothingMode.HighQuality;
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                using (var pen = new Pen(fillBrush ?? Brushes.Red, unitSize))
+                {
+
+                    g.DrawEllipse(pen, new Rectangle(unitSize, unitSize, unitSize, unitSize));
+                }
+            }
+            return BitmapCursor.CreateBmpCursor(bmp);
+        }
+
+        public static Cursor CreateFromBitmapSource(BitmapSource source)
+        {
+            var bitmap = BitmapSourceToBitmap(source);
+            return BitmapCursor.CreateBmpCursor(bitmap);
+        }
+        private static Bitmap BitmapSourceToBitmap(BitmapSource source)
+        {
+            using (var stream = new MemoryStream())
+            {
+                var e = new BmpBitmapEncoder();
+                e.Frames.Add(BitmapFrame.Create(source));
+                e.Save(stream);
+
+                var bmp = new Bitmap(stream);
+
+                return bmp;
+            }
+        }
+    }
+    /// <summary>
+    /// This class allow you create a Cursor form a Bitmap
+    /// </summary>
+    internal class BitmapCursor : SafeHandle
+    {
+        public override bool IsInvalid => handle == (IntPtr)(-1);
+
+        public static Cursor CreateBmpCursor(Bitmap cursorBitmap)
+        {
+
+            var c = new BitmapCursor(cursorBitmap);
+
+            return CursorInteropHelper.Create(c);
+        }
+        protected BitmapCursor(Bitmap cursorBitmap)
+            : base((IntPtr)(-1), true)
+        {
+            handle = cursorBitmap.GetHicon();
+        }
+        protected override bool ReleaseHandle()
+        {
+            bool result = DestroyIcon(handle);
+
+            handle = (IntPtr)(-1);
+
+            return result;
+        }
+        [DllImport("user32")]
+        private static extern bool DestroyIcon(IntPtr hIcon);
+    }
+}
---
+++ WPFCustomCursorDemo/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Brush = System.Drawing.Brush;
+using Brushes = System.Drawing.Brushes;
+using Pen = System.Drawing.Pen;
+using Rectangle = System.Drawing.Rectangle;
+
+namespace WPFCustomCursorDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+    public MainWindow()
+    {
+        InitializeComponent();
+        MouseEnter += (s, e) =>
+        {
+            ShowMouseCursor(e);
+        };
+        MouseMove += (s, e) =>
+        {
+            ShowMouseCursor(e);
+        };
+        StylusMove += (s, e) =>
+        {
+            ShowNoneCursor();
+        };
+    }
+
+    private void ShowNoneCursor()
+    {
+        if (Cursor == Cursors.None)
+        {
+            return;
+        }
+        Cursor = Cursors.None;
+        Mouse.UpdateCursor();
+    }
+    private void ShowMouseCursor(MouseEventArgs e)
+    {
+        if (e.StylusDevice != null && e.StylusDevice.Id > -1)
+        {
+            return;
+        }
+        if (Cursor == GetFillCursor())
+        {
+            return;
+        }
+        Cursor = GetFillCursor();
+        Mouse.UpdateCursor();
+    }
+    private Cursor _fillCursor = null;
+    private Cursor GetFillCursor()
+    {
+        return _fillCursor ?? (_fillCursor = CursorHelper.CreateFillCursor());
+    }
+    }
+}
